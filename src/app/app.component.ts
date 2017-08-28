3bc4724e11ba53980115a9b1b7a3b8b6a455b93a
jgpawletko <jgpawletko@gmail.com>
2017-08-28 01:03:28 -0400

    end of part 1, with some tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <div>
+      <label>title: </label>
+      <input [(ngModel)]="title" placeholder="new title">
+    </div>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="new name">
+    </div>
+    `
 })
+
 export class AppComponent {
-  title = 'My Apx';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
+export class Hero {
+  id:   number;
+  name: string;
 }
